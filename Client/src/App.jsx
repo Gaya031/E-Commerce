722ffa62d9631174f04971e9555ce987200a3038
Nitish Kumar <kumarnitishbxr07@gmail.com>
2025-12-07 18:06:49 +0530

    Start routing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from './pages/LandingPage'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Features from './pages/Features'
+import Pricing from './pages/Pricing'
+import Testimonial from './pages/Testimonial'
 
 function App() {
 
 
   return (
-    <>
-      <div className=' bg-amber-200 '>
-        <h1 className='text-center'>Project Begin</h1>
-      </div>
-    </>
+    <Router>
+        <Routes>
+            <Route path="/" element={<LandingPage/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/features" element={<Features/>} />
+            <Route path="/pricing" element={<Pricing/>} />
+            <Route path="/testimonial" element={<Testimonial/>} />
+        </Routes>
+    </Router>
   )
 }
 
---
+++ Client/src/pages/Testimonial.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Testimonial = () => {
+  return (
+    <div>
+      I am testimonial page
+    </div>
+  )
+}
+
+export default Testimonial
